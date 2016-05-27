d4399a9965771cca5e2595da4120a93a2ea77e65
Will <student@MacBook-Pro-0690-Graham.local>
2016-05-27 10:02:51 -0400

    put in first prep question
    have a little bit of code but mostly just got it ready

---
+++ examp_prep_1._triangles/examp_prep_1._triangles/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  examp_prep_1._triangles
+//
+//  Created by Will on 2016-05-27.
+//  Copyright © 2016 Will graham and co. All rights reserved.
+//
+
+import Foundation
+
+var angle1 = 0
+var angle2 = 0
+var angle3 = 0
+
+var itterate : Bool = true
+
+while (itterate == true){
+    print("Angle 1?")
+    
+}
